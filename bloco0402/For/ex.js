6ea10c3332a9164c02d76bbd7bb6387739fbe3b5
Rodrigo Macedo <romaco1694@gmail.com>
2022-06-22 16:10:55 -0300

    add lições

---
+++ bloco0402/For/ex.js
@@ -0,0 +1,7 @@
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let produtos = 0; produtos< groceryList.length; produtos +=1){
+let produtoDaLista = "Comprar " +groceryList[produtos]+ "!";
+console.log(produtoDaLista)
+}
